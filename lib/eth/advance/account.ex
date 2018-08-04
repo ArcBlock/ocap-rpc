88bd2eb20c1ee4da648f0aac875711c8a768a45d
Tyr Chen <tyr.chen@gmail.com>
2018-08-03 19:56:13 -0700

    rename tx_sent to txs_sent

--- lib/eth/advance/account.ex
+++ lib/eth/advance/account.ex
@@ -5,17 +5,18 @@ defmodule OcapRpc.Internal.EthAccount do
   alias OcapRpc.Converter
   alias OcapRpc.Eth.{Account, Chain, Contract, Trace, Transaction}
 
-  def tx_sent(address, num_blocks, block_offset),
+  def txs_sent(address, num_blocks, block_offset),
     do: get_transactions(address, num_blocks, block_offset, :sent)
 
-  def tx_received(address, num_blocks, block_offset),
+  def txs_received(address, num_blocks, block_offset),
     do: get_transactions(address, num_blocks, block_offset, :received)
 
   def get_by_address(address) do
     %{
       balance: Account.balance(address),
       address: address,
-      is_contract: Contract.total_supply(address) != 0
+      is_contract: Contract.total_supply(address) != 0,
+      number_txs_sent: Account.num_tx_sent(address)
     }
   end
 
@@ -38,6 +39,8 @@ defmodule OcapRpc.Internal.EthAccount do
     |> ProperCase.to_camel_case()
     |> Trace.filter()
     |> Enum.reject(fn item -> is_nil(item.transaction_hash) end)
+    # TODO(tchen): need a better interface for this
+    |> Enum.take(20)
     |> Enum.map(fn item -> Transaction.get_by_hash(item.transaction_hash) end)
   end
 end
