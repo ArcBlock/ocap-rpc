5b4c10a650a74c38ab869ea01a72fa746f18420f
Lei Zhou <lei@arcblock.io>
2018-08-16 22:11:10 -0700

    replace httpoison with tesla

--- lib/btc/rpc.ex
+++ lib/btc/rpc.ex
@@ -5,29 +5,32 @@ defmodule OcapRpc.Internal.BtcRpc do
   require Logger
   use Tesla
 
-  def call(method, params) when is_atom(method) do
-    config = Application.get_env(:ex_bitcoin, :conn)
+  plug(Tesla.Middleware.Retry, delay: 500, max_retries: 3)
+
+  if Application.get_env(:ocap_rpc, :env) not in [:test] do
+    plug(Tesla.Middleware.Timeout, timeout: 5_000)
+  end
+
+  def call(method, args) do
+    config = :ocap_rpc |> Application.get_env(:btc) |> Keyword.get(:conn)
     %{hostname: hostname, port: port, user: user, password: password} = config
 
-    # Logger.debug("Bitcoin RPC request for method: #{method}, params: #{inspect(params)}")
-
-    params = PoisonedDecimal.poison_params(params)
-
-    data = %{jsonrpc: "2.0", method: to_string(method), params: params, id: 1}
-
-    headers = [Authorization: "Basic " <> Base.encode64(user <> ":" <> password)]
+    body = get_body(method, args)
+    headers = [{"Authorization", "Basic " <> Base.encode64(user <> ":" <> password)}]
 
     result =
       post(
         "http://#{hostname}:#{to_string(port)}/",
-        Jason.encode!(data),
-        headers
+        Jason.encode!(body),
+        headers: headers
       )
 
     case result do
       {:ok, %{status: 200, body: body}} ->
-        %{"error" => nil, "result" => result} = Jason.decode!(body)
-        {:ok, result}
+        case Jason.decode!(body) do
+          %{"error" => nil, "id" => _, "result" => result} -> result
+          %{"error" => %{"code" => code, "message" => msg}} -> handle_error(code, msg)
+        end
 
       {:ok, %{status: code, body: body}} ->
         handle_error(code, body)
@@ -53,5 +56,88 @@ defmodule OcapRpc.Internal.BtcRpc do
       {:error, {:invalid, _token, _pos}} ->
         {:error, %{status: status, error: error, code: nil}}
     end
+  end
+
+  def resp_hook(resp, type \\ nil) do
+    case type do
+      :mem_pool_transaction -> convert_map_to_list_and_append_tx_id(resp)
+      :get_raw_transaction -> append_tx(resp)
+      :list_address_groupings -> list_to_map_and_append_key(resp)
+      :get_blockchain_info -> append_name(resp)
+      _ -> resp
+    end
+  end
+
+  defp convert_map_to_list_and_append_tx_id(resp) do
+    Enum.map(resp, fn {key, value} -> Map.put(value, "tx_id", key) end)
+  end
+
+  defp append_tx(resp) do
+    tx = call("decoderawtransaction", [resp["hex"]])
+    Map.merge(tx, resp)
+  end
+
+  defp append_name(resp) do
+    %{"bip9_softforks" => bip9_softforks} = resp
+
+    case is_nil(bip9_softforks) do
+      false ->
+        Map.put(resp, "bip9_softforks", append_bip9_softforks_name(bip9_softforks))
+
+      _ ->
+        resp
+    end
+  end
+
+  defp append_bip9_softforks_name(bip9_softforks) do
+    Enum.map(bip9_softforks, fn {key, value} -> Map.put(value, "name", key) end)
+  end
+
+  defp list_to_map_and_append_key(resp) do
+    Enum.map(resp, fn groupings ->
+      Enum.map(groupings, fn address_details ->
+        convert_list_to_map(address_details)
+        |> IO.inspect()
+      end)
+    end)
+  end
+
+  defp convert_list_to_map(address_details) do
+    [address, balance, account] = address_details
+
+    %{}
+    |> Map.put("address", address)
+    |> Map.put("balance", OcapRpc.Converter.btc_to_satoshi(balance))
+    |> Map.put("account", account)
+  end
+
+  defp get_body(method, args) do
+    case is_list(List.first(args)) do
+      true -> encode_many(method, args)
+      _ -> encode_single(method, args)
+    end
+  end
+
+  defp encode_single(method, args) do
+    %{
+      method: method,
+      params: args,
+      id: 1,
+      jsonrpc: "2.0"
+    }
+  end
+
+  defp encode_many(method, args) do
+    args
+    |> List.first()
+    |> Enum.with_index(1)
+    |> Enum.map(fn {item, idx} ->
+      %{
+        method: method,
+        params: item,
+        id: idx,
+        jsonrpc: "2.0"
+      }
+    end)
   end
 end
