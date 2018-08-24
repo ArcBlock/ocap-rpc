7e12ea3004ae3d9db9284cbf8affcabaa64c44b9
Lei <lei@arcblock.io>
2018-08-24 15:43:12 -0700

    support basic btc rpc (#18)
    * bump version
    
    * replace httpoison with tesla
    
    * replace httpoison with tesla
    
    * add retry support
    
    * support basic btc rpc
    
    * bump version

--- lib/btc/btc_gen.ex
+++ lib/btc/btc_gen.ex
@@ -1,23 +1,69 @@
 defmodule OcapRpc.Internal.BtcCodeGen do
   @moduledoc """
-  Ethereum specific code generator
+  Bitcoin specific code generator
   """
   alias OcapRpc.Internal.{BtcRpc, Extractor, Parser}
+  alias UtilityBelt.CodeGen.DynamicModule
+  require DynamicModule
 
-  def gen_method(name, method, args, result, doc) do
-    data = quote_rpc_call(name, args, method, result, doc)
-    AtomicMap.convert(data, safe: false)
+  @mem_pool_transaction [
+    "getrawmempool",
+    "getmempoolancestors",
+    "getmempooldescendants",
+    "getmempoolentry"
+  ]
+  @get_raw_transaction "getrawtransaction"
+  @list_address_groupings "listaddressgroupings"
+  @get_blockchain_info "getblockchaininfo"
+
+  def gen_method(name, method, args, result, opts) do
+    cond do
+      Enum.member?(@mem_pool_transaction, method) ->
+        quote_rpc_call(name, args, method, result, [{:action, :mem_pool_transaction} | opts])
+
+      String.starts_with?(method, @get_raw_transaction) ->
+        quote_rpc_call(name, args, method, result, [{:action, :get_raw_transaction} | opts])
+
+      String.starts_with?(method, @list_address_groupings) ->
+        quote_rpc_call(name, args, method, result, [{:action, :list_address_groupings} | opts])
+
+      String.starts_with?(method, @get_blockchain_info) ->
+        quote_rpc_call(name, args, method, result, [{:action, :get_blockchain_info} | opts])
+
+      true ->
+        quote_rpc_call(name, args, method, result, opts)
+    end
   end
 
-  defp quote_rpc_call(name, args, method, result, doc) do
+  defp quote_rpc_call(name, args, method, result, opts) do
+    action = Access.get(opts, :action)
+    doc = Access.get(opts, :doc)
+    mod_type = get_type(opts)
+
     quote do
       @doc unquote(doc)
       def unquote(String.to_atom(name))(unquote_splicing(Parser.gen_args(args))) do
-        args = Enum.map(binding(), fn {_, v} -> v end)
+        values = Enum.map(unquote(args), fn k -> Keyword.get(binding(), k) end)
 
-        response = BtcRpc.call(unquote(method), args)
-        Extractor.process(response, unquote(Macro.escape(result)))
+        unquote(method)
+        |> BtcRpc.call(values)
+        |> BtcRpc.resp_hook(unquote(action))
+        |> Extractor.process(unquote(Macro.escape(result)), unquote(mod_type))
       end
+    end
+  end
+
+  defp get_type(opts) do
+    type = Access.get(opts, :type)
+
+    case type do
+      nil ->
+        nil
+
+      _ ->
+        mod_name = DynamicModule.gen_module_name(:ocap_rpc, "Btc", "Type", Recase.to_pascal(type))
+
+        String.to_atom("Elixir.#{mod_name}")
     end
   end
 end
